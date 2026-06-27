ea9257bbf7f442d36104a7fc66bcf63a9ecb4f05
vibhaastrip <vibhaastrip@gmail.com>
2026-06-28 00:41:57 +0100

    moving avg bs

--- src/strategies/moving_average.py
+++ src/strategies/moving_average.py
@@ -6,3 +6,19 @@ from src.base_strategy import BaseStrategy
 class MovingAverageCrossStrategy(BaseStrategy):
     def __init__(self,short_window=50, long_window=200, name="MA Crossover"):
         super().__init__(name)
+
+        self.short_window = short_window
+        self.long_window = long_window
+
+    def generate_signals(self, data: pd.DataFrame) -> pd.DataFrame:
+        df = data.copy()
+
+        df['short_ma'] = df['close'].rolling(window=self.short_window).mean()
+        df['long_ma'] = df['close'].rolling(window-self.long_window).mean()
+
+        df['signal'] = 0
+
+        df.loc[df['short_ma']> df['long_ma'], 'signal'] = 1 
+        df.loc[df['short_ma']< df['long_ma'], 'signal'] = -1
+
+        return df
